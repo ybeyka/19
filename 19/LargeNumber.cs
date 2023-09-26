a8abf92958bcc18b3bfe0c89729440e1f56798ba
ybey <ybeyky@gmail.com>
2023-09-26 14:22:51 +0300

    Добавьте файлы проекта.

---
+++ 19/LargeNumber.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Text;
+
+public class LargeNumber
+{
+    private readonly string number;
+
+    public LargeNumber(string number)
+    {
+        this.number = number;
+    }
+
+    public static LargeNumber Add(LargeNumber a, LargeNumber b)
+    {
+        StringBuilder result = new StringBuilder();
+        int cc = 0;
+
+        string num1 = a.number.PadLeft(Math.Max(a.number.Length, b.number.Length), '0');
+        string num2 = b.number.PadLeft(Math.Max(a.number.Length, b.number.Length), '0');
+
+        for (int i = num1.Length - 1; i >= 0; i--)
+        {
+            int sum = (num1[i] - '0') + (num2[i] - '0') + cc;
+            result.Insert(0, (char)('0' + sum % 10));
+            cc = sum / 10;
+        }
+
+        if (cc != 0)
+            result.Insert(0, (char)('0' + cc));
+
+        return new LargeNumber(result.ToString());
+    }
+
+    public static LargeNumber Subtract(LargeNumber a, LargeNumber b)
+    {
+        if (string.Compare(a.number, b.number) < 0)
+            throw new ArgumentException("");
+
+        StringBuilder result = new StringBuilder();
+        int bb= 0;
+
+        string num1 = a.number.PadLeft(Math.Max(a.number.Length, b.number.Length), '0');
+        string num2 = b.number.PadLeft(Math.Max(a.number.Length, b.number.Length), '0');
+
+        for (int i = num1.Length - 1; i >= 0; i--)
+        {
+            int diff = (num1[i] - '0') - (num2[i] - '0') - bb;
+            if (diff < 0)
+            {
+                diff += 10;
+                bb = 1;
+            }
+            else
+                bb = 0;
+
+            result.Insert(0, (char)('0' + diff));
+        }
+
+        return new LargeNumber(result.ToString().TrimStart('0'));
+    }
+
+    public LargeNumber Multiply(LargeNumber other)
+    {
+        string num1 = this.number;
+        string num2 = other.number;
+
+        int len1 = num1.Length;
+        int len2 = num2.Length;
+        int[] result = new int[len1 + len2];
+
+        for (int i = len1 - 1; i >= 0; i--)
+        {
+            int c = 0;
+            int n1 = num1[i] - '0';
+
+            for (int j = len2 - 1; j >= 0; j--)
+            {
+                int n2 = num2[j] - '0';
+                int sum = n1 * n2 + result[i + j + 1] + c;
+                c = sum / 10;
+                result[i + j + 1] = sum % 10;
+            }
+            result[i] += c;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        foreach (int num in result)
+        {
+            sb.Append(num);
+        }
+
+        string resultString = sb.ToString().TrimStart('0');
+        return new LargeNumber(resultString == "" ? "0" : resultString);
+    }
+
+    public override string ToString() => number;
+}
---
+++ 19/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введiть перше велике число:");
+        string input1 = Console.ReadLine();
+        LargeNumber num1 = new LargeNumber(input1);
+
+        Console.WriteLine("Введiть друге велике число:");
+        string input2 = Console.ReadLine();
+        LargeNumber num2 = new LargeNumber(input2);
+
+        Console.WriteLine("Виберiть операцiю (+, -, *):");
+        string operation = Console.ReadLine().ToLower();
+
+        LargeNumber result;
+        switch (operation)
+        {
+            case "+":
+                result = LargeNumber.Add(num1, num2);
+                Console.WriteLine($"Результат: {num1} + {num2} = {result}");
+                break;
+            case "-":
+                result = LargeNumber.Subtract(num1, num2);
+                Console.WriteLine($"Результат: {num1} - {num2} = {result}");
+                break;
+            case "*":
+                result = num1.Multiply(num2);
+                Console.WriteLine($"Результат: {num1} * {num2} = {result}");
+                break;
+            default:
+                Console.WriteLine("Невiдома операцiя");
+                break;
+        }
+    }
+}
